82a678224c9aad5b84ea5392039aad8b18715a1d
Dominic <jxdlam@gmail.com>
2016-06-06 12:01:22 +0800

    Created questions for the quiz

--- quiz.js
+++ quiz.js
@@ -1,9 +1,30 @@
 /* global $ */
 
-$(function () {
+function Question (prompt, answers, correctAnswerIndex) {
+  this.prompt = prompt;
+  this.choices = answers;
+  this.correctChoice = correctAnswerIndex;
+}
 
+// using the new keyword and the constructor we can create questions for the quiz
+var question1 = new Question('the question', ['answer a', 'answer b', 'answer c', 'answer d'], 0);
+
+// we can create an object to represent all of the settings and scores for the quiz
+var quiz = {
+  currentQuestion: 0,
+  questions: [question1, question1, question1, question1],
+  isGameOver: false,
+  player1Points: 0,
+  player2Points: 0
+};
+
+$(function () {
+  $('button').on('click', function (index) {
+    playTurn(index.target.id);
+  });
 });
-var currentTurn;
+
+
 
 //  #restart()
 //  It should restart the game so it can be played again.
@@ -14,8 +35,8 @@ function restart () {
 //  # isGameOver()
 //  It should return a true or false if the quiz is over.
 function isGameOver () {
-  if (numberOfQuestions) return false;
-  return true;
+  if (currentQuestion > numberOfQuestions) return true;
+  return false;
 }
 
 // # whoWon()
@@ -28,32 +49,32 @@ function whoWon () {
 
 // # numberOfQuestions()
 // It should return an integer that is the number of questions in a game
+
 function numberOfQuestions () {
-  return 1;
+  return quiz.questions.length;
 }
 
 // # currentQuestion()
 // It should return an integer that is the zero-based index of the current question in the quiz
 function currentQuestion () {
-  return 0;
+  return quiz.currentQuestion;
 }
 
 // # numberOfAnswers()
 // It should return an integer that is the number of choices for the current question
 function numberOfAnswers () {
-  return 2;
+  return 4;
 }
 
 // # correctAnswer()
 // It should return an integer that is the zero-based index the correct answer for the currrent question
 function correctAnswer () {
-  return 0;
+  return 1;
 }
 
 // # playTurn(choice)
 // It should take a single integer, which specifies which choice the current player wants to make.
 // It should return a boolean true/false if the answer is correct.
-function playTurn () {
-  if (!correctAnswer()) return false;
-  if (isGameOver());
+function playTurn (choice) {
+  console.log(choice);
 }
